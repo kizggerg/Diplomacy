f5adf34febef02f88fa015d214f2a79c516c199b
kizggerg <gregorygzik@gmail.com>
2017-04-05 12:26:52 -0700

    Map Completely Represented

---
+++ src/ubc/projects/tests/Board_Tests.java
@@ -0,0 +1,11 @@
+package ubc.projects.tests;
+
+//todo: Test board
+
+/**
+ * Created by greggzik on 2017-04-05.
+ * Tests the functionality and correctness of the board.
+ */
+public class Board_Tests {
+
+}
