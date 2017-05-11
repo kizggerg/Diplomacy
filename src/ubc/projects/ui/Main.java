3b3781e939dca62c800aa2a6b36a2aab76881d1b
kizggerg <gregorygzik@gmail.com>
2017-05-10 17:17:21 -0700

    Began GUI implementation in Main

--- src/ubc/projects/ui/Main.java
+++ src/ubc/projects/ui/Main.java
@@ -1,8 +1,34 @@
 package ubc.projects.ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Constructs the GUI
+ */
+public class Main  extends Application{
+    Label title;
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    /**
+     * Starts the JavaFX Application.
+     * @param primaryStage     The stage for the Game
+     * @throws Exception       Thrown when an exception occurs.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Diplomacy");
+        title = new Label("Diplomacy Title Screen");
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 300, 250);
+        layout.getChildren().add(title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
